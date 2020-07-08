9d18f281f1a9be919aeb357373bfb2420d9401bc
Tejas Satpalkar <tejasrs2@illinois.edu>
2020-07-08 12:27:33 -0500

    Finished First version of project

--- app.py
+++ app.py
@@ -1,2 +1,70 @@
 import tweepy
 import os, requests
+import time
+
+from decouple import config
+
+def Setup():
+    temp = {"CONSUMER_KEY": config("CONSUMER_KEY"),
+            "CONSUMER_SECRET": config("CONSUMER_SECRET"),
+            "ACCESS_TOKEN": config("ACCESS_TOKEN"),
+            "ACCESS_TOKEN_SECRET": config("ACCESS_TOKEN_SECRET")
+            }
+    auth = tweepy.OAuthHandler(config("CONSUMER_KEY"), config("CONSUMER_SECRET"))
+    auth.set_access_token(config("ACCESS_TOKEN"), config("ACCESS_TOKEN_SECRET"))
+    api = tweepy.API(auth)
+    return api
+
+
+def makeTweet():
+    # set the url and the message
+    query = requests.get("https://api.nasa.gov/planetary/apod",
+                         params={"api_key": config("NASA"),
+                                 "hd": True}).json()
+    # From this json I must get the 'hdurl', media_type, 'title'
+    media_type = query['media_type']
+    if media_type == 'image':
+        print(query)
+        title = query['title']
+        url = query['hdurl']
+        message = f"{title}: copyright: {query['copyright']} {query['explanation']}"
+        message = message[:276]
+        message += '...'
+        return url, message
+    else:
+        print("fail")
+        return None
+
+
+def main():
+    api = Setup()
+    INTERVAL = 60*60*24
+    filename = 'temp.jpg'
+    while True:
+        apod = makeTweet()
+        if apod is not None:
+            url = apod[0]
+            message = apod[1]
+
+            # Rest of function is copy and pasted from StackOverflow user Brobin
+            # https://stackoverflow.com/questions/31748444/how-to-update-twitter-status-with-image-using-image-url-in-tweepy
+            request = requests.get(url, stream=True)
+            if request.status_code == 200:
+                with open(filename, 'wb') as image:
+                    for chunk in request:
+                        image.write(chunk)
+
+                api.update_with_media(filename, status=message)
+                os.remove(filename)
+            else:
+                print("Unable to download image")
+
+        else:
+            print('not an image')
+        time.sleep(INTERVAL)
+
+
+if __name__ == "__main__":
+    main()
+
+
